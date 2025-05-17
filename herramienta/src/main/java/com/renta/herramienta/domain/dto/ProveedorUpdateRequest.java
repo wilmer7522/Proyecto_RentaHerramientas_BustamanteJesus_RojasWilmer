e1b5a3f0ef7a8d6c1c438a5ca90eb250e9a42824
JesusBustamante <jesusgustamante08@gmail.com>
2025-05-16 20:32:40 -0500

    feat: :sparkles: Supplier Patch

---
+++ herramienta/src/main/java/com/renta/herramienta/domain/dto/ProveedorUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.renta.herramienta.domain.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProveedorUpdateRequest {
+    private String nombre;
+    private String telefono;
+    private String direccion;
+    private String nit;
+}
